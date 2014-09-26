e37748b8c47e4ac7286565722fc4ed6d95bdca06
remon <remmony@gmail.com>
2014-09-26 14:27:10 +0200

    ss

--- index.php
+++ index.php
@@ -3,40 +3,188 @@
 <html>
 
 <head>
-    <title> EndoDev</title>
-        <meta name="description" content="EndoDev for web development" />
-  <meta name="robots" content="index,follow"/>
-<meta name="googlebot" content="noarchive"/>
-  <link rel="image_src" href="http://www.endodev.com/images/cairo.jpg" />
-  
-  <meta  content="http://www.endodev.com/images/cairo.jpg"  property="og:image">
+     <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-   <link href='http://fonts.googleapis.com/css?family=Josefin+Slab' rel='stylesheet' type='text/css'>
+    <title> EndoDev</title>
+   <link href='http://fonts.googleapis.com/css?family=Luckiest+Guy' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Droid+Sans|Lato' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/3.2.0/css/font-awesome.min.css" />
     <link rel="stylesheet" href="css/boot.css" />
+    <link rel="stylesheet" href="css/page.css" />
     <link rel="stylesheet" href="css/links.css" />
+    <style type="text/css">
+       #map-canvas { 
+           height: 80%;margin-top: 5%; width: 50%;float: right; margin-right: 50px;
+           border: 5px solid white;
+           border-radius: 5px;
+       
+       }
+        .myinfo{
+            padding-top: 180px;
+            padding-left: 10px;
+            padding-right: 10px;
+        }
+   #map-canvas img { max-width:none; }
+    </style>
    
     <link rel="stylesheet" href="css/main.css" />
     
-    
+    <script type="text/javascript"
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDxmAmU1B6NXUUUp95m4q5N7FSm--_1Mpw"></script>
     <script src="js/jquery.js" ></script>
-
+<script src="js/page.js" ></script>
     <script src="js/main.js" ></script>
+  <script type="text/javascript">
+      var myLatlng = new google.maps.LatLng(27.2049046,33.8486507);
+      function initialize() {
+          
+        var mapOptions = {
+          center: myLatlng,
+          zoom: 15,
+            draggable :true,
+   mapTypeControl: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+    },
+    zoomControl: true,
+    zoomControlOptions: {
+      style: google.maps.ZoomControlStyle.DEFAULT
+    },
+            scrollwheel :false,
+         
+             mapTypeId: google.maps.MapTypeId.ROADMAP,
+     
+    streetViewControl: false
+        };
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+            var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title:"EndoDev"
+});
+      }
+    
+      google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
     </head>
     
     <body >
-        <img src="images/cairo.jpg" class="fixed_p cov"  />
-        <img src="images/cloud2.png" class="cloudy fixed_p" />
-        <img src="images/airplane2.png" class="fixed_p air" />
-        <img src="images/boat2.png" class="fixed_p boat" />
-        <img src="images/contact_us.png" class="fixed_p contact" />
-        <img src="images/work.png" class="fixed_p work" />
         
-       <div class="fixed_p name_c">
-        EndoDev
-        </div>
-        <script>
-        </script>
+        <div class="fixed_p my_bar wmiya">
+            <a href="/">
+            <img src="images/endo_logo.png" class="our_logo lefty" alt="endodev.com"/>
+                </a>
+               <ul id="myMenu" class="_mgt8 lefty luc">
+    <li data-menuanchor="Home" class="active"><a href="#Home">Home</a></li>
+    <li data-menuanchor="Techs"><a href="#Techs">Technologies</a></li>
+    
+    <li data-menuanchor="Contact"><a href="#Contact">Contact</a></li>
+</ul>
+            </div>
+ 
+      <div id="pagepiling">
+          
+    <div class="section">
+         
+          <img src="images/mobiles.png" class="my_mob lefty" />
+         <h2 class="bui lefty centery luc"> We build for the web</h2>
+        
+        <p class="my_p">
+        We do amazing and ambitious web applications for desktop and mobile , 
+            
+            The <span class="idea">Idea</span>  is yours ,and <span class="we">We</span>  do the rest
+        
+        </p>
+          </div>
+    <div class="section">
+          <h2 class="my_t abs wmiya whity centery luc">
+              Technologies we use
+        </h2>
+        <ul class="techs">
+        <li>
+            <a href="#">
+                <span class="flip whity wmiya centery inline_b _5br"> Ruby on Rails</span>
+            <img src="images/railslogo.png" />
+            </a>
+            
+            </li>
+             <li>
+            <a href="#">
+                <span class="flip whity wmiya centery inline_b _5br"> Coffee Script</span>
+            <img src="images/coffe.png" />
+            </a>
+            
+            </li>
+        
+               <li>
+            <a href="#">
+                <span class="flip whity wmiya centery inline_b _5br"> jQuery</span>
+            <img src="images/jquery.png" />
+            </a>
+            
+            </li>
+                   <li>
+            <a href="#">
+                <span class="flip whity wmiya centery inline_b _5br"> AngularJs</span>
+            <img src="images/angular.png" />
+            </a>
+            
+            </li>
+                   <li>
+            <a href="#">
+                <span class="flip whity wmiya centery inline_b _5br"> EmberJs</span>
+            <img src="images/ember.png" />
+            </a>
+            
+            </li>
+                      <li>
+            <a href="#">
+                <span class="flip whity wmiya centery inline_b _5br"> Php</span>
+            <img src="images/php.png" />
+            </a>
+            
+            </li>
+                         <li>
+            <a href="#">
+                <span class="flip whity wmiya centery inline_b _5br"> Html5 & CSS3</span>
+            <img src="images/htm5.png" />
+            </a>
+            
+            </li>
+                          <li class="mobs _30">
+            <a href="#">
+                <span class="flip whity wmiya centery inline_b _5br"> Mobile Apps</span>
+            <img src="images/mobs2.png" />
+            </a>
+            
+            </li>
+                            <li style="font-size: 2em;" class="l_t centery andm">
+            <a href="#" class="whity">
+            and more
+            </a>
+            
+            </li>
+        </ul>       
+          </div>
+          
+    
+    <div class="section">
+
+        
+       <div id="map-canvas"></div>
+        
+        <span class="myinfo centery blocky lefty _40 whity  _15f">
+        EndoDev for web development and internet marketing
+            <br/ >
+            Sheraton Road Hurghada-Egypt
+            <br/ >
+            email: info@endodev.com
+        
+        </span>
+  
+        
+          </div>
+</div>
     </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,70 +1,15 @@
-$(document).ready(function(){
-    var air =$(".air"),
-         l_left = parseInt(air.css("left")),
-    left = $(document).width() - 150,
-        dur = 9000;
-    function move_air(){
-    air.css("left",l_left +"px").animate({
-        "right" :  "50px",
-        left : left +"px"
+$(document).ready(function() {
+    $('#pagepiling').pagepiling({
+         sectionsColor: ['#fff','#3399CC', '#4BBFC3', '#2C3E50'],
+        //sectionsColor: ['#fff','#3399CC', '#4BBFC3', '#2C3E50', '#3399CC', '#000'],
+        anchors: ['Home', 'Techs',  'Contact'],
+    menu: '#myMenu',
+       navigation: {
+            'textColor': '#000',
+            'bulletsColor': '#000',
+            'position': 'right',
+            'tooltips': ['Home', 'Technologies',  'Contact']
+        }   
         
-        },dur ,"linear",function(){
-           air.css("left",l_left +"px").animate({
-                "right" :  "50px",
-                left : left +"px"
-               },dur,"linear", move_air);
-            
-        });//end of animate
-        
-    }
-    move_air();
-                
-               var cloud =$(".cloudy"),
-         l_right = parseInt(cloud.css("right")),
-    right = $(document).width() - 150,
-        dur = 9000;
-    function move_cloud(){
-    cloud.fadeOut(100).fadeIn(100).css("right",l_right +"px").animate({
-        
-        right : right +"px"
-        
-        },dur ,"linear",function(){
-         cloud.fadeOut(100).fadeIn(100).css("right",l_right +"px").animate({
-               
-        right : right +"px"
-               },dur,"linear", move_cloud);
-            
-        });//end of animate
-        
-    }
-    
-    move_cloud();     
-           
-                 var boot =$(".boat"),
-         l_bottom = parseInt(boot.css("bottom")),
-                     newbottom =50,
-    top = $(document).height() -  l_bottom,
-                     nw = $(document).height() -newbottom ,
-        ndur = 2000;
-    function move_boot(){
-    boot.fadeOut(10).fadeIn(10).css({
-        top : top +"px"
-        }).animate({
-          top : nw +"px",
-        
-        
-        },ndur ,"linear",function(){
-         boot.fadeOut(10).fadeIn(10).css({
-        top : top +"px"
-        }).animate({
-               top : nw +"px"
-               },ndur,"linear", move_boot);
-            
-        });//end of animate
-        
-    }
-    
-    move_boot();  
-    
-    
-});//end of ready
+    });
+});
